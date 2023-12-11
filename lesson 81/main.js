258e62e9e47d3c4e90305a9c52aa78affdb6f442
tukpairah <136407516+tukpairah@users.noreply.github.com>
2023-12-11 21:52:39 +0500

    Add files via upload

---
+++ lesson 81/main.js
@@ -0,0 +1,15 @@
+const numbers = [3, 8, 12, 5, 18, 7, 10, 6, 15, 22];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers [i] % 2 === 0) {
+    console.log(numbers [i]);
+  }
+}
+
+const rainbow = ["Красный", "Оранжевый", "Жёлтый", "Зелёный", "Голубой", "Синий", "Фиолетовый"];
+
+for (let i = rainbow.length - 1; i >= 0; i--) {
+    console.log(rainbow [i]);
+  }
+
+
